f560786a760aec84201ddd273ddc3c0ab549ff00
Laia Mia Perez <laiperez1020@gmail.com>
2023-04-14 14:17:07 -0300

    implementando mercado pago

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ const routesUser = require('./routes/users')
 const bodyParser = require('body-parser')
 const routeCompras = require('./routes/compras')
 const payRouter = require('./routes/mp')
+const cors = require("cors");
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   const allowedOrigins = ['http://localhost:3000', 'https://www.mercadopago.com.ar']
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
+//   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
 
 app.use('/productos', route )
 app.use('/usuarios', routesUser )
--- routes/mp.js
+++ routes/mp.js
@@ -1,9 +1,73 @@
 const {Router} = require('express')
 const mercadopago = require('mercadopago')
+const { compra, usuario, producto } = require('../db')
 require('dotenv').config()
 
 const payRouter = Router()
-let usuarioDB = process.env.ACCES_TOKEN_MP
+let token = process.env.ACCES_TOKEN_MP
+
+payRouter.post('/', async (req, res) => {
+    
+    mercadopago.configure({
+        access_token: token
+    })
+    let preferenceId;
+
+    //declaro la preferencia
+    let preference = {
+        items: [
+            {
+                title: "Dummy Item",
+                description: "Multicolor Item",
+                currency_id: "$",
+                quantity: 1,
+                unit_price: 10
+            }
+        ],
+        back_urls: {
+			"success": "http://localhost:3001/pagar/feedback",
+			"failure": "http://localhost:3001/pagar/feedback",
+			"pending": "http://localhost:3001/pagar/feedback"
+		},
+		auto_return: "approved",
+    }
+
+    //traigo todo de la BDD
+    // let cliente = await compra.findAll({
+    //     where: {usuarioId: "6f7b03c4-7d83-4114-9ab0-d8e1aedf621b"},
+    //     include: [
+    //         { model: usuario },
+    //         { model: producto }
+    //     ]
+    // })
+
+    //recorro para hacer llenar los datos de la preferencia
+    // await cliente.forEach((co) => {
+    //     preference.items.push({
+    //         title: co.dataValues.producto.dataValues.nombre,
+    //         quantity: co.dataValues.cantidad !== 0 ? co.dataValues.cantidad : 1,
+    //         unit_price: co.dataValues.producto.dataValues.precio
+    //     })
+    // })
+
+    //creo la prefencia
+    await mercadopago.preferences.create(preference)
+    .then((r) =>  {
+        console.log(r.body)
+        preferenceId = r.body.id
+        res.status(200).send(preferenceId)
+    }) 
+    .catch((err) => console.log(err))
+
+})
+
+payRouter.get('/feedback', function (req, res) {
+	res.json({
+		Payment: req.query.payment_id,
+		Status: req.query.status,
+		MerchantOrder: req.query.merchant_order_id
+	});
+});
 
 
 module.exports = payRouter
